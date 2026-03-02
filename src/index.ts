855eb87aedf6632a38093729470512026583d63d
Anastasiia <anastasiia.perederii@gmail.com>
2026-03-02 22:16:40 +0200

    add task code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+type DeepReadonly<T> =
+    T extends (...args: unknown[]) => unknown
+        ? T
+        : T extends readonly unknown[]
+            ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+            : T extends object
+                ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+                : T;
+
+type DeepRequireReadonly<T> =
+    T extends (...args: unknown[]) => unknown
+        ? T
+        : T extends readonly unknown[]
+            ? { readonly [K in keyof T]-?: DeepRequireReadonly<T[K]> }
+            : T extends object
+                ? { readonly [K in keyof T]-?: DeepRequireReadonly<T[K]> }
+                : T;
+
+type UpperCaseKeys<T> = {
+    [K in keyof T as K extends string ? Uppercase<K> : K]: T[K];
+};
+
+type ObjectToPropertyDescriptor<T> = {
+    [K in keyof T]: TypedPropertyDescriptor<T[K]>;
+};
